5a72bcd4ab85a59b449de44ff4885f97ae7c184f
quihp <quiquihuynh@gmail.com>
2025-03-02 16:16:09 +0700

    Add tailwind + header

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,10 +12,10 @@ require("dotenv").config({
 });
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Qui's personal page`,
+    description: `This is my personal page including my CV, my blog, my interests and etc`,
+    author: `@quihp`,
+    siteUrl: `https://quihp.netlify.app/`,
   },
   plugins: [
     {
@@ -57,8 +57,9 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo/logo.png`, // This path is relative to the root of the site.
       },
     },
+    'gatsby-plugin-postcss'
   ],
 }
